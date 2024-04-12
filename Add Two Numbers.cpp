fc79bbd057e913bdbc2de40a641d06999f92216d
kush <119112478+ankush-singh2005@users.noreply.github.com>
2024-04-13 00:01:17 +0530

    Update Add Two Numbers.cpp

--- Add Two Numbers.cpp
+++ Add Two Numbers.cpp
@@ -11,14 +11,30 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+        //creating a new dummy LL which has only 0 as a node in it
         ListNode* dummy=new ListNode(0);
+
+        //assigning temp to dummy;
         ListNode* temp=dummy;
         int carry=0;
+
+        //checking here if l1 and l2 are null or not and also checking if carry exists
         while(l1!=NULL || l2!=NULL || carry){
-            int sum=(l1?l1->val:0)+(l2?l2->val:0l)+carry;
+
+            //here checking using terneray operator whether l1 and l2 exists or not if they do then add the value present or else add 0 and sum it all with the carry
+            int sum=(l1?l1->val:0)+(l2?l2->val:0)+carry;
+
+            //carry is equal to the sum/10
             carry=sum/10;
+
+            //making a new node for temp and sending the value there.. sum%10 because we dont want the value to exceed from 10
             temp->next=new ListNode(sum%10);
+
+            //changing temp to temp->next
             temp=temp->next;
+
+            //checking if l1 exists and if it does then moving on to the next node
             if(l1) l1=l1->next;
             if(l2) l2=l2->next;
             
